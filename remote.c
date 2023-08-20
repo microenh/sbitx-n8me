7dac8ba52a785f7c4e03a3dc5a226121e0c25d82
Ashhar Farhan <afarhan@gmail.com>
2023-08-20 15:43:29 -0400

    working on remote (telnet)

--- remote.c
+++ remote.c
@@ -24,26 +24,26 @@ static int incoming_ptr;
 #define MAX_LINE 30
 
 void remote_start(){
-  char buffer[MAX_DATA];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
+    char buffer[MAX_DATA];
+    struct sockaddr_in serverAddr;
+    struct sockaddr_storage serverStorage;
+    socklen_t addr_size;
 
-  welcome_socket = socket(PF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-  
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(8081);
-  serverAddr.sin_addr.s_addr = INADDR_ANY;
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+    welcome_socket = socket(PF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
-  /*---- Bind the address struct to the socket ----*/
-  bind(welcome_socket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(8081);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-  /*---- Listen on the socket, with 5 max connection requests queued ----*/
-  if(listen(welcome_socket,5)!=0)
-    printf("telnet listen() Error\n");
-  incoming_ptr = 0;
-	puts("#Remote is listening");
+    /*---- Bind the address struct to the socket ----*/
+    bind(welcome_socket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+    /*---- Listen on the socket, with 5 max connection requests queued ----*/
+    if(listen(welcome_socket,5)!=0)
+        printf("telnet listen() Error\n");
+    incoming_ptr = 0;
+    puts("#Remote is listening");
 }
 
 void remote_send(char *m){
@@ -53,10 +53,9 @@ void remote_send(char *m){
 // from https://andreasen.org/letters/vt100.txt
 
 void remote_init(){
-
-	remote_send("\033[1;1H");//goto 1,1
-	remote_send("\033[r"); //clear the scrollable area
-	remote_send("\033[2J"); //clear the screen
+	remote_send("\033[1;1H");   // goto 1,1
+	remote_send("\033[r");      // clear the scrollable area
+	remote_send("\033[2J");     // clear the screen
 	remote_send("\033[25;1r");
 
 	remote_send(VER_STR);
@@ -65,12 +64,12 @@ void remote_init(){
 
 void remote_write(char *message){
 	
-	if (data_socket < 0)
-		return;
+    if (data_socket < 0)
+        return;
 
-//	remote_send("\0337");
+    // remote_send("\0337");
 	int e = send(data_socket, message, strlen(message), 0);
-//	remote_send("\0338");
+    // remote_send("\0338");
 	if (e >= 0)
 		return;
 	close(data_socket);
@@ -78,49 +77,45 @@ void remote_write(char *message){
 }
 
 void remote_slice(){
-  struct sockaddr_storage server_storage;
-  socklen_t addr_size;
-  int e, len;
-  char buffer[1024];
+    struct sockaddr_storage server_storage;
+    socklen_t addr_size;
+    int e, len;
+    char buffer[1024];
 
-  if (data_socket == -1){
-    addr_size = sizeof server_storage;
-    e = accept(welcome_socket, (struct sockaddr *) &server_storage, &addr_size);
-    if (e == -1)
-      return;
-    puts("Accepted telnet connection\n");
-    incoming_ptr = 0;
-    data_socket = e;
-    fcntl(data_socket, F_SETFL, fcntl(data_socket, F_GETFL) | O_NONBLOCK);
-
-		// init the console
-		remote_init();
-  }
-  else { 
-    len = recv(data_socket, buffer, sizeof(buffer), 0);
-    if (len > 0){
-      buffer[len] = 0;
-			printf("Received on remote : [%s]\n", buffer);
-			//strip of the last \r or \n
-			buffer[strcspn(buffer, "\r\n")] = 0;
-			if (buffer[0] == '?'){
-				char response[2000];
-				get_field_value_by_label(buffer+1, response);
-				strcat(response, "\n");
-				remote_write(response);
-			}	
-			else if(strlen(buffer))	
-				remote_execute(buffer);
-    }
-    else {
-      //e = errno();
-      if (errno == EAGAIN || errno == EWOULDBLOCK)
-        return;
-      //for other errors, just close the socket
-			puts("telnet connection dropped. Restarting to listen ..."); 
-      close(data_socket);
-      data_socket = -1;      
-    }
-  } 
+    if (data_socket == -1){
+        addr_size = sizeof server_storage;
+        e = accept(welcome_socket, (struct sockaddr *) &server_storage, &addr_size);
+        if (e == -1)
+            return;
+        puts("Accepted telnet connection\n");
+        incoming_ptr = 0;
+        data_socket = e;
+        fcntl(data_socket, F_SETFL, fcntl(data_socket, F_GETFL) | O_NONBLOCK);
+        // init the console
+        remote_init();
+    } else { 
+        len = recv(data_socket, buffer, sizeof(buffer), 0);
+        if (len > 0){
+            buffer[len] = 0;
+            // strip of the last \r or \n
+            buffer[strcspn(buffer, "\r\n")] = 0;
+            printf("Received on remote : [%s]\n", buffer);
+            if (buffer[0] == '?'){
+                char response[2000];
+                get_field_value_by_label(buffer+1, response);
+                strcat(response, "\n");
+                remote_write(response);
+            } else if(strlen(buffer))	
+                remote_execute(buffer);
+        } else {
+            // e = errno();
+            if (errno == EAGAIN || errno == EWOULDBLOCK)
+                return;
+            // for other errors, just close the socket
+            puts("telnet connection dropped. Restarting to listen ..."); 
+            close(data_socket);
+            data_socket = -1;      
+        }
+    } 
 }
 
